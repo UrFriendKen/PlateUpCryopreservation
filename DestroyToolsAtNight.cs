bad65a2c37d1f63395b5c1768dec5b2858ef7411
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2023-07-14 10:59:51 +0800

    init

---
+++ DestroyToolsAtNight.cs
@@ -0,0 +1,31 @@
+﻿using Kitchen;
+using KitchenMods;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace KitchenCryopreservation
+{
+    public class DestroyToolsAtNight : StartOfNightSystem, IModSystem
+    {
+        EntityQuery Tools;
+        protected override void Initialise()
+        {
+            base.Initialise();
+            Tools = GetEntityQuery(typeof(CToolInUse), typeof(CHeldBy));
+        }
+
+        protected override void OnUpdate()
+        {
+            using NativeArray<Entity> entities = Tools.ToEntityArray(Allocator.Temp);
+            using NativeArray<CHeldBy> heldBys = Tools.ToComponentDataArray<CHeldBy>(Allocator.Temp);
+
+            for (int i = entities.Length - 1; i > -1; i--)
+            {
+                if (heldBys[i].Holder == default || !Has<CPreservesContentsOvernight>(heldBys[i].Holder))
+                {
+                    EntityManager.DestroyEntity(entities[i]);
+                }
+            }
+        }
+    }
+}
---
+++ MarkToolStoragePreserves.cs
@@ -0,0 +1,39 @@
+﻿using Kitchen;
+using KitchenMods;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace KitchenCryopreservation
+{
+    public class MarkToolStoragePreserves : DaySystem, IModSystem
+    {
+        EntityQuery ToolStorages;
+
+        protected override void Initialise()
+        {
+            base.Initialise();
+            ToolStorages = GetEntityQuery(typeof(CToolStorage), typeof(CHeldBy));
+        }
+
+        protected override void OnUpdate()
+        {
+            using NativeArray<Entity> entities = ToolStorages.ToEntityArray(Allocator.Temp);
+            using NativeArray<CHeldBy> heldBys = ToolStorages.ToComponentDataArray<CHeldBy>(Allocator.Temp);
+
+            for (int i = 0; i < entities.Length; i++)
+            {
+                Entity entity = entities[i];
+                CHeldBy heldBy = heldBys[i];
+
+                if (heldBy.Holder != default && Has<CPreservesContentsOvernight>(heldBy.Holder))
+                {
+                    Set<CPreservesContentsOvernight>(entity);
+                }
+                else if (Has<CPreservesContentsOvernight>(entity))
+                {
+                    EntityManager.RemoveComponent<CPreservesContentsOvernight>(entity);
+                }
+            }
+        }
+    }
+}
